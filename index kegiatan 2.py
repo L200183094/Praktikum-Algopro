d12347f11a17ec2c0fb1e0a2d216a41152be2e42
L200183094 <44944022+L200183094@users.noreply.github.com>
2019-01-01 11:41:35 +0700

    Add files via upload

---
+++ index kegiatan 2.py
@@ -0,0 +1,26 @@
+print "<!DOCTYPE html>"
+print
+print """<html><head><title>Data Diri</title>
+</head>
+<body align='left'>
+<h1 align="center">Data Diri</h1>
+<table border='0' align="center">
+	<tr>
+		<td rowspan="5">
+		<img src="apa aja.jpg"></img></td>
+		<td>Nama : Farah Husna Paramadina</td>
+	</tr>
+	<tr>
+		<td>Alamat tinggal: Klaten</td>
+	</tr>
+	<tr>
+		<td>Tempat, Tanggal Lahir: Klaten, 21 mei 2000<td>
+	</tr>
+	<tr>
+		<td>Tempat Wisata favorit: Rumah</td>
+	</tr>
+	<tr>
+		<td>motto : Happy</td>
+	</tr>
+</table>
+</body></html>"""
